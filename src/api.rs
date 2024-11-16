102421871e80663069af248bc69bcc439bfe4e1a
Nick Fang <fang.nicholas@gmail.com>
2024-11-16 16:12:42 -0600

    posting a message works now.

--- src/api.rs
+++ src/api.rs
@@ -1,12 +1,14 @@
 use reqwest::blocking::Client;
 use serde_json::json;
+use std::env;
 
 pub fn send_message(api_key: String, message: &Vec<[i32; 22]>) {
     let client = Client::new();
-    let url = "http://vestaboard.local:7000/local-api/message";
+    let ip_address = env::var("IP_ADDRESS").expect("IP_ADDRESS not set");
+    let url = format!("http://{}:7000/local-api/message", ip_address);
     let body = json!(message);
 
-    let res = client.post(url).header("X-Vestaboard-Local-Api-Key", api_key).json(&body).send();
+    let res = client.post(&url).header("X-Vestaboard-Local-Api-Key", api_key).json(&body).send();
 
     match res {
         Ok(response) => println!("Response: {:?}", response),
@@ -16,9 +18,10 @@ pub fn send_message(api_key: String, message: &Vec<[i32; 22]>) {
 
 pub fn get_message(api_key: String) {
     let client = Client::new();
-    let url = "http://vestaboard.local:7000/local-api/message";
+    let ip_address = env::var("IP_ADDRESS").expect("IP_ADDRESS not set");
+    let url = format!("http://{}:7000/local-api/message", ip_address);
 
-    let res = client.get(url).header("X-Vestaboard-Local-Api-Key", api_key).send();
+    let res = client.get(&url).header("X-Vestaboard-Local-Api-Key", api_key).send();
 
     match res {
         Ok(response) =>
