0e39eee5881581e3da8085039f3637c23ccc6cbd
Nick Fang <fang.nicholas@gmail.com>
2024-12-29 11:20:36 -0600

    Using forcast instead of current api endpoint so I can get access to high and low temp for the day.  Made structs for the new data that's brought in from the new endpoint.  Reformatted weather display.

--- src/widgets/weather/weather.rs
+++ src/widgets/weather/weather.rs
@@ -7,51 +7,52 @@ use serde::Deserialize;
 struct WeatherResponse {
     current: Current,
     location: Location,
+    forecast: Forecast,
 }
 
 #[derive(Deserialize, Debug)]
 struct Location {
-    country: String,
-    lat: f64,
-    localtime_epoch: i64,
-    localtime: String,
-    lon: f64,
     name: String,
     region: String,
+    country: String,
+    lat: f64,
+    lon: f64,
     tz_id: String,
+    localtime_epoch: i64,
+    localtime: String,
 }
 
 #[derive(Deserialize, Debug)]
 struct Current {
-    cloud: i32,
-    condition: Condition,
-    dewpoint_c: f64,
-    dewpoint_f: f64,
-    feelslike_c: f64,
-    feelslike_f: f64,
-    gust_kph: f64,
-    gust_mph: f64,
-    heatindex_c: f64,
-    heatindex_f: f64,
-    humidity: i32,
-    is_day: i32,
     last_updated_epoch: i64,
     last_updated: String,
-    precip_in: f64,
-    precip_mm: f64,
-    pressure_in: f64,
-    pressure_mb: f64,
     temp_c: f64,
     temp_f: f64,
-    uv: f64,
-    vis_km: f64,
-    vis_miles: f64,
-    wind_degree: i32,
-    wind_dir: String,
+    is_day: i32,
+    condition: Condition,
     wind_kph: f64,
     wind_mph: f64,
+    wind_degree: i32,
+    wind_dir: String,
+    pressure_in: f64,
+    pressure_mb: f64,
+    precip_in: f64,
+    precip_mm: f64,
+    humidity: i32,
+    cloud: i32,
+    feelslike_c: f64,
+    feelslike_f: f64,
     windchill_c: f64,
     windchill_f: f64,
+    heatindex_c: f64,
+    heatindex_f: f64,
+    dewpoint_c: f64,
+    dewpoint_f: f64,
+    vis_km: f64,
+    vis_miles: f64,
+    uv: f64,
+    gust_kph: f64,
+    gust_mph: f64,
 }
 
 #[derive(Deserialize, Debug)]
@@ -61,16 +62,106 @@ struct Condition {
     code: i32,
 }
 
+#[derive(Deserialize, Debug)]
+struct Forecast {
+    forecastday: Vec<ForecastDay>,
+}
+
+#[derive(Deserialize, Debug)]
+struct ForecastDay {
+    astro: Astro,
+    date: String,
+    date_epoch: i64,
+    day: Day,
+    hour: Vec<Hour>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Astro {
+    sunrise: String,
+    sunset: String,
+    moonrise: String,
+    moonset: String,
+    moon_phase: String,
+    moon_illumination: i32,
+    is_moon_up: i32,
+    is_sun_up: i32,
+}
+
+#[derive(Deserialize, Debug)]
+struct Day {
+    maxtemp_c: f64,
+    maxtemp_f: f64,
+    mintemp_c: f64,
+    mintemp_f: f64,
+    avgtemp_c: f64,
+    avgtemp_f: f64,
+    maxwind_kph: f64,
+    maxwind_mph: f64,
+    totalprecip_in: f64,
+    totalprecip_mm: f64,
+    totalsnow_cm: f64,
+    avgvis_km: f64,
+    avgvis_miles: f64,
+    avghumidity: i32,
+    daily_will_it_rain: i32,
+    daily_chance_of_rain: i32,
+    daily_will_it_snow: i32,
+    daily_chance_of_snow: i32,
+    condition: Condition,
+    uv: f64,
+}
+
+#[derive(Deserialize, Debug)]
+struct Hour {
+    time_epoch: i64,
+    time: String,
+    temp_c: f64,
+    temp_f: f64,
+    is_day: i32,
+    condition: Condition,
+    wind_kph: f64,
+    wind_mph: f64,
+    wind_degree: i32,
+    wind_dir: String,
+    pressure_in: f64,
+    pressure_mb: f64,
+    precip_in: f64,
+    precip_mm: f64,
+    snow_cm: f64,
+    humidity: i32,
+    cloud: i32,
+    feelslike_c: f64,
+    feelslike_f: f64,
+    windchill_c: f64,
+    windchill_f: f64,
+    heatindex_c: f64,
+    heatindex_f: f64,
+    dewpoint_c: f64,
+    dewpoint_f: f64,
+    will_it_rain: i32,
+    chance_of_rain: i32,
+    will_it_snow: i32,
+    chance_of_snow: i32,
+    vis_km: f64,
+    vis_miles: f64,
+    gust_kph: f64,
+    gust_mph: f64,
+    uv: f64,
+}
+
 pub async fn get_weather() -> Result<Vec<String>, reqwest::Error> {
     dotenv().ok();
     let weather_api_key = env::var("WEATHER_API_KEY").expect("WEATHER_API_KEY not set");
     println!("Weather API Key: {}", weather_api_key);
     let client = Client::new();
-    let url =
+    let url_current =
         format!("https://api.weatherapi.com/v1/current.json?key={}&q=austin", weather_api_key);
-    println!("URL: {}", url);
+    let url_forecast =
+        format!("https://api.weatherapi.com/v1/forecast.json?key={}&q=austin&days=1&aqi=no&alerts=no", weather_api_key);
+    println!("URL: {}", url_forecast);
     // return Ok(());
-    let res = client.get(&url).send().await;
+    let res = client.get(&url_forecast).send().await;
 
     match res {
         Ok(response) => {
@@ -79,12 +170,27 @@ pub async fn get_weather() -> Result<Vec<String>, reqwest::Error> {
                     println!("Response: {:?}", json);
                     let mut weather_description = Vec::new();
                     weather_description.push(format!("{}", json.location.localtime.to_lowercase()));
-                    weather_description.push(format!("temp:      {}", json.current.temp_f));
                     weather_description.push(
-                        format!("condition: {:?}", json.current.condition.text.to_lowercase())
+                        format!(
+                            "{}° l:{}° h:{}°",
+                            json.current.temp_f,
+                            json.forecast.forecastday[0].day.mintemp_f,
+                            json.forecast.forecastday[0].day.maxtemp_f
+                        )
                     );
-                    weather_description.push(format!("precip:    {}", json.current.precip_in));
-                    weather_description.push(format!("pressure:  {}", json.current.pressure_in));
+                    weather_description.push(
+                        format!(
+                            "{:?}",
+                            json.current.condition.text.replace("\"", "").to_lowercase()
+                        )
+                    );
+                    weather_description.push(
+                        format!(
+                            "with {}in of rain",
+                            json.forecast.forecastday[0].day.totalprecip_in
+                        )
+                    );
+                    weather_description.push(format!("pressure: {}", json.current.pressure_in));
                     println!("{:?}", weather_description);
                     Ok(weather_description)
                 }
