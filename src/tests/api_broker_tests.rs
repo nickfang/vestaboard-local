cca2dcc4d7d44c6a2acab01f6d6c0b274c094faa
Nick Fang <fang.nicholas@gmail.com>
2025-01-14 00:02:24 -0600

    Adding api_broker tests.

--- src/tests/api_broker_tests.rs
+++ src/tests/api_broker_tests.rs
@@ -1,6 +1,6 @@
 #[path = "../api_broker.rs"]
 mod api_broker;
-use api_broker::to_codes;
+use api_broker::{ to_codes, display_message };
 
 #[cfg(test)]
 #[test]
@@ -42,4 +42,37 @@ fn test_message_with_colors() {
     let message = "ROYGBVWK";
     let expected_codes = Some(vec![63, 64, 65, 66, 67, 68, 69, 70]);
     assert_eq!(to_codes(message), expected_codes);
+}
+
+#[test]
+fn test_display_message() {
+    let message = vec!["hello".to_string(), "world".to_string()];
+    let codes = display_message(message).unwrap();
+    assert_eq!(codes, [
+        [8, 5, 12, 12, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [23, 15, 18, 12, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+}
+
+#[test]
+fn test_display_message_all_characters() {
+    let message = vec![
+        "ROYGBVKW".to_string(),
+        "abcdefghijklmnopqrstuv".to_string(),
+        "wxyz1234567890".to_string(),
+        "!@#$()-+&=;:'\"%,./?°".to_string()
+    ];
+    let codes = display_message(message).unwrap();
+    assert_eq!(codes, [
+        [63, 64, 65, 66, 67, 68, 70, 69, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+        [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 0, 0, 0, 0, 0, 0, 0, 0],
+        [37, 38, 39, 40, 41, 42, 44, 46, 47, 48, 49, 50, 52, 53, 54, 55, 56, 59, 60, 62, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
 }
